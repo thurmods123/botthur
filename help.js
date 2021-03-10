eb1ab8b0f6a886c1d859fc05fc9afc82ed7dfccc
thurmods123 <80352462+thurmods123@users.noreply.github.com>
2021-03-10 11:02:31 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,125 @@
+const help = (prefix) => {
+	return `
+
+MEU WHATSAPP:http://wa.me/5564992551969
+
+aqui vão estar todos os comandos do ThurBot
+
+🥶 *Comandos para figurinhas* 🐊
+
+🔰 Comando : *${prefix}sticker* ou *${prefix}stiker*
+🔰 (transformar imagem/gif/vídeo em figurinha)
+🔰 uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+
+🔰 Comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+🔰 (dsclp esse comando não funciona)
+🔰 uso : responder imagem ou enviar imagem com legenda/n
+
+🔰 Comando : *${prefix}toimg*
+🔰 serve para : transformar figurinha em imagem (não funciona com vídeos)
+🔰 uso : adesivo de resposta\n
+
+🔰 Comando : *${prefix}tsticker* ou *${prefix}tstiker*
+🔰 útil em : converter texto em adesivo
+🔰 uso : *${prefix}tsticker seu texto aqui*\n
+
+🥶✅ *Comandos memes* 🐊
+
+🔰 Comando : *${prefix}meme*
+ útil em : mandar meme [ *inglês* ]
+🔰 uso : basta emviar o comando\n
+
+🔰 Comando : *${prefix}memeindo*
+🔰 serve para : mandar imagens aleatórias de meme [indo] *sla para que serve isso*
+🔰 uso : basta enviar o comando\n
+
+🥶🐊 *Outros comandos* 🥶
+
+🔰 Comando : *${prefix}gtts pt*
+🔰 serve para : converter texto em fala/áudio
+🔰 uso : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
+
+    ✅COMANDOS ALEATÓRIOS✅
+
+🔰 Comando : *${prefix}url2img*
+🔰 útil em : (tirar screenshots da web)
+🔰 uso : *${prefix}url2img [tipe] [url]*\n
+🔰 Comando : *${prefix}simi*
+🔰 útil em : responder sua mensagem por simi
+🔰 uso : *${prefix}simi sua mensagem*\n
+
+🔰 Comando : *${prefix}ocr*
+🔰 útil em : pegar o texto da foto e lhe enviar
+🔰 uso : responder imagem ou enviar mensagem com legenda\n
+
+🔰 Comando : *${prefix}wait*
+🔰 útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+🔰 uso : responder imagem ou enviar imagem com legenda\n
+
+🔰 Comando : *${prefix}setprefix*
+🔰 útil em : alterar o prefixo do bot
+🔰 uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+🔰 Nota : Este comando só pode ser usado pelo proprietário do bot\n
+
+🥶 *comandos só funciona em grupos* 🐊
+
+Comando : *${prefix}linkgroup*
+🔰 útil em : enviar o link do grupo
+🔰 uso : basta enviar o comando\n
+
+🔰 Comando : *${prefix}tagall*
+🔰 serve para : (marcar todos os membros do grupo, incluindo administradores)
+🔰 uso : basta enviar o comando\n
+🔰 Nota : *Este comando só pode ser usado se você for um administrador* do grupo\n
+
+🔰 Comando : *${prefix}simih*
+🔰 útil em : ativar o modo simi no grupo
+🔰 uso : *${prefix}simih 1* para ativar o modo simi e *${prefix}simih 0* para desativar o modo simi
+🔰 Nota : Este comando só pode ser usado se você for um administrador do grupo\n
+
+──❉ * ALGUMAS SÓ FUNCIONA SE O BOT E VC FOR ADM✅* ❉──
+
+🔰 *${prefix}promote @*
+(PROMOVER ALGUÉM PARA ADM)
+
+🔰 *${prefix}demote @*
+(TIRAR ADM DE ALGUÉM)
+
+🔰 *${prefix}kick @*
+(REMOVER ALGUÉM)
+
+🔰 *${prefix}welcome 1*
+(DAR BOAS VINDAS QUANDO ALGUÉM ENTRAR E QUANDO UMA PESSOA SAIR ELE VAI AVISAR)
+
+🔰 *${prefix}welcome 0*
+(DESATIVAR O RECUSSO DE BOAS VINDAS)
+
+🔰 *${prefix}clearall* (deleta todos os chats do wpp do bot)
+
+🔰 *${prefix}listadmins (lista dos admins)*
+(MARCAR TODOS OS ADMS)
+
+🔰 *${prefix}add (numero da pessoa)*
+(ADICIONAR ALGUÉM NO GRUPO)
+
+🔰 *${prefix}tagall 
+(MARCAR TODAS AS PESSOAS DO GRUPO)
+
+🔰 ${prefix}yt2mp3
+(não sei se tá funcionando, mais se estiver serve para baixar música)
+
+ MAIS INFORMAÇÕES:
+WHATSAPP: http://wa.me/5564992551969
+MEU 
+
+espero que vcs gostem que ta foda termina meus bot pq ta tudo sendo banido :( by cychris <3
+
+}
+
+exports.help = help
+
+
+
+
+
+
